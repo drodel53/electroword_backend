230e3ad313fd4c26d775e8ef766a1da0bace8b04
drodel53 <diegorodriguezdelgado04@gmail.com>
2025-05-15 09:15:11 +0200

    Editado archivo manifest

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+'name': 'ElectroWord API backend',
+'version': '1.0',
+'summary': 'API REST de backend para obtener productos y clientes en Odoo',
+'author': 'Diego Rodríguez Delgado',
+'category': 'API',
+'depends': ['base', 'sale', 'product', 'website'],
+'data': [],
+'icon': '/electroword_api/static/description/icon510.png',
+'installable': True,
+'application': False,
+}
